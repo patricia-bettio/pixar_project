364881f8329b9425599247b198fc3136c20d3ecd
pbettio <pfbettio@gmail.com>
2019-09-10 22:33:22 +0200

    JS- Years and basic data included (need to fix assignment of Decades and years)

--- javascript.js
+++ javascript.js
@@ -9,57 +9,48 @@ function getData() {
     .then(showData);
 }
 
-
-//2- first interaction - how-to find each element
+//example first interaction - how-to find each element
 /*function showData(data) {
   data.feed.entry.forEach(oneMovie);
 }
-
-
-
 function oneMovie(item){
     console.log(item.gsx$starrating.$t)
 }
 */
 
-
-//3- showing basic info: movie details/name
+//3- adding functions to display data
 
 function showData(data){
-
-
-    data.feed.entry.forEach(createCategories);
     data.feed.entry.forEach(oneMovie);
+    data.feed.entry.forEach(newTitle);
 }
 
+//4- Create categories
 
+function newTitle(decades){
+    console.log(decades.gsx$decade.$t);
+    const section = document.createElement("section");
+    const header = document.createElement("h1");
+    header.textContent=decades.gsx$decade.$t;
+    section.setAttribute("id", decades.gsx$decade.$t);
+    section.appendChild(header);
+    document.querySelector("main").appendChild(section);
+}
 
-
+//3-basic info
 function oneMovie(item){
-    console.log(item);
+
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
     copy.querySelector(".movie_title").textContent=item.gsx$filmname.$t;
     copy.querySelector(".short_description").textContent=item.gsx$shortsdescription.$t;
+
+
     document.querySelector(".movieslist").appendChild(copy);
+
 }
 
 
-
-
-//4- Create categories + assign each Item to its category
-
-
-
-function createCategories(category){
-
-    const section = document.createElement("section");
-    const header = document.createElement("h1");
-    header.textContent=category.gsx$decade.$t;
-    section.setAttribute("id", category.gsx$decade.$t);
-    section.appendChild(header);
-    document.querySelector("main").appendChild(section);
-}
 
 
 
